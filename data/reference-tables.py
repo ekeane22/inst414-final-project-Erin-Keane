7dede0e654bc2f57d4ae08ac0f7663599a2c4a7f
ekeane22 <ekeane@terpmail.umd.edu>
2024-07-27 22:45:25 -0400

    Getting there!

--- data/reference-tables.py
+++ data/reference-tables.py
@@ -7,8 +7,8 @@ import numpy as np
 #need to figure out a more efficient way
 
 data_dict_df = pd.read_csv("Data Dictionary_Predicting Flight Delays 2022.csv")
-data_dict_df.head
+data_dict_df.head()
 
 data_dict2_df = pd.read_csv("Data Dictionary_US Weather Events (2016 - 2022).csv")
-data_dict2_df.head
+data_dict2_df
 
